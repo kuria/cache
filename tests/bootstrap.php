c79c410cc4ab7feac7034658bffc94b9033a8e9d
ShiraNai7 <shiranai7@hotmail.com>
2017-05-08 14:23:51 +0200

    Require PHPUnit as a dev dependency

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,4 @@
 <?php
 
-if (is_file(__DIR__ . '/../vendor/autoload.php')) {
-    $classLoader = require __DIR__ . '/../vendor/autoload.php';
-    $classLoader->addPsr4('Kuria\\Cache\\', __DIR__);
-}
+$classLoader = require __DIR__ . '/../vendor/autoload.php';
+$classLoader->addPsr4('Kuria\\Cache\\', __DIR__);
